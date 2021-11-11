1f1f93d8617ca5375e2dac885ac4b2fab6ddcc55
Andrea Agricola <andrea.agricolajob@gmail.com>
2021-11-11 11:59:10 +0100

    first deploy on gitpages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,22 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router"
-import {HomeComponent} from "@app/modules/dashboard/pages/home/home.component";
-import {PageNotFoundComponent} from "@app/page-not-found/page-not-found.component";
-import {AuthGuard} from "@app/core/auth.guard";
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '@app/modules/dashboard/pages/home/home.component';
+import { PageNotFoundComponent } from '@app/page-not-found/page-not-found.component';
+import { AuthGuard } from '@app/core/auth.guard';
 
 const routes: Routes = [
-    { path: 'home', component: HomeComponent },
-    { path: 'projects',
-    loadChildren: () => import('./modules/project/project.module').then(m => m.ProjectModule)
-    },
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: '**', component: PageNotFoundComponent}
+  { path: 'home', component: HomeComponent },
+  {
+    path: 'projects',
+    loadChildren: () =>
+      import('./modules/project/project.module').then((m) => m.ProjectModule),
+  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes)
-    ],
-    exports: [
-        RouterModule
-    ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-
 export class AppRoutingModule {}
